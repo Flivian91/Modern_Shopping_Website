dedf7303d5b814477589783284e4978b6d5f5c0e
Murchoid <kamaucoder@gmail.com>
2024-05-07 21:10:46 +0300

    Changed the links to ask for GET  request throught the server

--- app.js
+++ app.js
@@ -9,6 +9,37 @@ app.use(express.static(path.join(__dirname, '/views/public')));
 app.get('/', (req, res) => {
     res.render('index.ejs')
 })
+
+app.get('/store', (req, res) => {
+    res.render('store.ejs')
+})
+
+app.get('/account', (req, res) => {
+    res.render('create-account.ejs')
+})
+
+app.get('/checkout', (req, res) => {
+    res.render('checkout.ejs')
+})
+
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard.ejs')
+})
+
+app.get('/product-overview', (req, res) => {
+    res.render('product-quickviews.ejs')
+})
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.get('/account-manage', (req, res) => {
+    res.render('account-manage.ejs')
+})
+
+
+
 app.listen(port, () => {
     console.log("Server open at port + ", port)
 })
